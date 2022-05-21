34a44b75a16216905eac5b72557d47c6b44dd908
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-21 17:16:34 +0300

    add attributes to group entity

--- src/Entity/Group.php
+++ src/Entity/Group.php
@@ -2,20 +2,59 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\GroupRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: GroupRepository::class)]
-#[ORM\Table(name: '`group`')]
+#[ORM\Table(name: '`groups`')]
+#[ApiResource]
 class Group
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    private $id;
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\NotBlank(message: "The name '{{ value }}' is empty.")]
+    public string $name;
+
+    #[ORM\ManyToMany(targetEntity: User::class, inversedBy: 'groups')]
+    public Collection $users;
+
+    public function __construct()
+    {
+        $this->users = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getUsers(): Collection
+    {
+        return $this->users;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'name' => $this->getName()
+        ];
     }
 }
