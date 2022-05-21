89e4c74ae5987a72fd0df65d78fd1b8d8d761630
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-21 17:10:15 +0300

    add attributes to user entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,19 +3,110 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Symfony\Component\Validator\Constraints as Assert;
+use ApiPlatform\Core\Annotation\ApiResource;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
-#[ORM\Table(name: '`user`')]
-class User
+#[ORM\Table(name: '`users`')]
+#[ORM\HasLifecycleCallbacks]
+#[UniqueEntity(fields: 'email', message: 'Email already taken')]
+#[ApiResource(normalizationContext: ['skip_null_values' => null])]
+class User implements \JsonSerializable
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
-    private $id;
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    #[Assert\Length(min: 2, max: 50)]
+    #[Assert\NotBlank]
+    public string $name;
+
+    #[ORM\Column(type: 'string', length: 255, unique: true)]
+    #[Assert\Length(min: 2, max: 50)]
+    #[Assert\Email(message: "The email '{{ value }}' is not a valid email.")]
+    public string $email;
+
+    #[ORM\ManyToMany(targetEntity: Group::class, inversedBy: 'users')]
+    #[ORM\JoinTable(name: 'users_groups')]
+    #[ORM\JoinColumn(name: 'user_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    #[ORM\InverseJoinColumn(name: 'group_id', referencedColumnName: 'id', onDelete: 'CASCADE')]
+    public Collection $groups;
+
+    public function __construct()
+    {
+        $this->groups = new ArrayCollection();
+    }
+
+    public function __toString(): string
+    {
+        return trim($this->getName().' '.$this->getEmail());
+    }
 
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getGroups(): Collection
+    {
+        return $this->groups;
+    }
+
+    public function addGroup(Group $group): self
+    {
+        if (!$this->groups->contains($group)) {
+            $this->groups[] = $group;
+        }
+
+        return $this;
+    }
+
+    public function removeGroup(Group $group): self
+    {
+        if ($this->groups->contains($group)) {
+            $this->groups->removeElement($group);
+        }
+
+        return $this;
+    }
+
+    public function removeGroups(): self
+    {
+        $this->groups->clear();
+        return $this;
+    }
+
+    public function jsonSerialize(): array
+    {
+        return [
+            'name'  => $this->getName(),
+            'email' => $this->getEmail()
+        ];
     }
 }
