95f727125b31c3c25cd29f2d18827f1f10a8cff4
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-26 10:22:26 +0300

    setup api-platform on production

--- public/index.php
+++ public/index.php
@@ -16,8 +16,10 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
  */
 $trustedProxies = $_SERVER['TRUSTED_PROXIES'] ?? $_ENV['TRUSTED_PROXIES'] ?? false;
 $trustedProxies = $trustedProxies ? explode(',', $trustedProxies) : [];
-if($_SERVER['APP_ENV'] == 'prod') $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
-if($trustedProxies) {
+if (isset($_SERVER['APP_ENV']) && $_SERVER['APP_ENV'] === 'prod') {
+    $trustedProxies[] = $_SERVER['REMOTE_ADDR'];
+}
+if ($trustedProxies) {
     Request::setTrustedProxies($trustedProxies, Request::HEADER_X_FORWARDED_AWS_ELB);
 }
 ####################################################################################################
