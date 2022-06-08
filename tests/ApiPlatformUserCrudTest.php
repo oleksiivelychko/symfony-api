f7b347f821e11c17bace6f117c08861565680014
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-06-08 08:31:26 +0300

    cover api platform for user entity by tests

---
+++ tests/ApiPlatformUserCrudTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Tests;
+
+use ApiPlatform\Core\Bridge\Symfony\Bundle\Test\ApiTestCase;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\DecodingExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+class ApiPlatformUserCrudTest extends ApiTestCase
+{
+    static int $currentUserId = 0;
+
+    private string $apiEndpoint = '/api/users';
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function testCreateUser(): void
+    {
+        $response = static::createClient()->request('POST', $this->apiEndpoint, [
+            'json' => [
+                'name' => 'user-0',
+                'email' => 'user-0@email.com',
+            ]
+        ]);
+
+        static::$currentUserId = json_decode($response->getContent())->id;
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonContains([
+            '@id' => $this->apiEndpoint.'/'.static::$currentUserId,
+            'name' => 'user-0',
+            'email' => 'user-0@email.com',
+        ]);
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function testGetUser(): void
+    {
+        static::createClient()->request('GET', $this->apiEndpoint.'/'.static::$currentUserId);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonContains([
+            '@id' => $this->apiEndpoint.'/'.static::$currentUserId,
+        ]);
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws DecodingExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function testUpdateUser(): void
+    {
+        static::createClient()->request('PUT', $this->apiEndpoint.'/'.static::$currentUserId, [
+            'json' => [
+                'name' => 'user-00',
+            ]
+        ]);
+
+        $this->assertResponseIsSuccessful();
+        $this->assertJsonContains([
+            '@id' => $this->apiEndpoint.'/'.static::$currentUserId,
+            'name' => 'user-00',
+        ]);
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function testDeleteUser(): void
+    {
+        static::createClient()->request('DELETE', $this->apiEndpoint.'/'.static::$currentUserId);
+        $this->assertResponseIsSuccessful();
+    }
+}
