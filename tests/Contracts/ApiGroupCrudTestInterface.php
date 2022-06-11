aa6982a540c4d625850422b3121765249763f8c3
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-06-11 10:21:03 +0300

    implement api v2 group test contracts

---
+++ tests/Contracts/ApiGroupCrudTestInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Tests\Contracts;
+
+interface ApiGroupCrudTestInterface
+{
+    public function testCreateGroup(): void;
+
+    public function testGetGroup(): void;
+
+    public function testUpdateGroup(): void;
+
+    public function testDeleteGroup(): void;
+}
