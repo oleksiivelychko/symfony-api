a5efb02fda4d5282b48f274273339ee86212503a
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-06-18 13:58:33 +0300

    to make it abstract

--- src/Controller/RestfulController.php
+++ src/Controller/RestfulController.php
@@ -2,10 +2,9 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class RestfulController extends AbstractController
+abstract class RestfulController extends AbstractController
 {
     public const ENTITY_NOT_FOUND        = 'Entity not found';
     public const ENTITY_HAS_BEEN_CREATED = 'Entity has been created';
@@ -14,19 +13,6 @@ class RestfulController extends AbstractController
     public const UNPROCESSABLE_ENTITY    = 'Unprocessable entity';
     public const EMAIL_IS_NOT_VALID      = 'Email is not valid email address';
     public const EMAIL_ALREADY_TAKEN     = 'Email already taken';
-    public const EMPTY_REQUEST_DATA      = 'Empty request data';
-
-    protected function transformJsonBody(Request $request): Request
-    {
-        $data = json_decode($request->getContent(), true);
-
-        if ($data === null) {
-            return $request;
-        }
-
-        $request->request->replace($data);
-        return $request;
-    }
 
     protected function unprocessableExceptionMessage(\Exception $e): string
     {
