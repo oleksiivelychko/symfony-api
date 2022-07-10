94825dd74234400dda59ecb9ee04f06bcf399e22
Oleksii Velychko <alexeyv@herdwatch.com>
2022-07-10 19:25:23 +0300

    define default timezone

--- src/Kernel.php
+++ src/Kernel.php
@@ -8,4 +8,12 @@ use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    public function boot(): void
+    {
+        parent::boot();
+
+        $timezone = $this->getContainer()->getParameter('timezone');
+        date_default_timezone_set($timezone);
+    }
 }
