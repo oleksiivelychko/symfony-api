b3eac85b9c6ed2ad57bbd5460f9a6389275afbfc
Oleksii Velychko <alexeyv@herdwatch.com>
2022-07-24 11:28:17 +0300

    load fixtures with password hasher

--- prepare_db.sql
+++ prepare_db.sql
@@ -1,3 +1,4 @@
+--- PostgreSQL
 DELETE FROM groups;
 ALTER SEQUENCE groups_id_seq RESTART;
 UPDATE groups SET id = DEFAULT;
@@ -5,3 +6,9 @@ UPDATE groups SET id = DEFAULT;
 DELETE FROM users;
 ALTER SEQUENCE users_id_seq RESTART;
 UPDATE users SET id = DEFAULT;
+
+--- SQLite
+DELETE FROM groups;
+delete from sqlite_sequence where name='groups';
+DELETE FROM users;
+delete from sqlite_sequence where name='users';
--- src/DataFixtures/ApiFixtures.php
+++ src/DataFixtures/ApiFixtures.php
@@ -6,21 +6,43 @@ use App\Entity\Group;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class ApiFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $userPasswordHasher;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher)
+    {
+        $this->userPasswordHasher = $userPasswordHasher;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $group01 = new Group('group-01');
         $manager->persist($group01);
 
-        $user = new User('user-01', 'user-01@email.com', $group01);
+        $user = new User();
+        $user->setName('user-01');
+        $user->setEmail('user-01@email.com');
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword($user, 'secret')
+        );
+        $user->setRoles('USER');
+        $user->addGroups($group01);
         $manager->persist($user);
 
         $group02 = new Group('group-02');
         $manager->persist($group02);
 
-        $user = new User('user-02', 'user-02@email.com', $group01, $group02);
+        $user = new User();
+        $user->setName('user-02');
+        $user->setEmail('user-02@email.com');
+        $user->setPassword(
+            $this->userPasswordHasher->hashPassword($user, 'secret')
+        );
+        $user->setRoles('USER');
+        $user->addGroups($group01, $group02);
         $manager->persist($user);
 
         $manager->flush();
