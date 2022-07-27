49b8bd2a2a190e49e051a4e8947267beb006fb16
Oleksii Velychko <alexeyv@herdwatch.com>
2022-07-27 08:28:15 +0300

    create contains roles constraint

---
+++ src/Validator/ContainsRoles.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Validator;
+
+use Attribute;
+use Symfony\Component\Validator\Constraint;
+
+#[Attribute]
+class ContainsRoles extends Constraint
+{
+    public string $message = 'assert.roles.invalid';
+
+    public function validatedBy(): string
+    {
+        return 'assert.contains_roles';
+    }
+}
